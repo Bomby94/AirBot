01a9c1865ac9e1ced05be1ab84d27b06e010fe57
Fulvio <fulvio.bombara@gmail.com>
2024-09-21 12:12:34 +0200

    pip requirements

--- AirForce.py
+++ AirForce.py
@@ -2,10 +2,7 @@ import time
 
 import pyautogui
 import pygetwindow
-from pyautogui import locateOnScreen, center, moveTo, leftClick, ImageNotFoundException
-import pyscreeze
-from pygetwindow import Win32Window
-from pyscreeze import Box
+from pyautogui import locateOnScreen, moveTo, leftClick
 
 import keyboard
 
@@ -20,10 +17,8 @@ class Giochino:
     def focusGameWindow(self):
         self.gameWindow.activate()
 
-
     def moveToPlanes(self):
         pyautogui.leftClick(self.windowX(0.5), self.windowY(0.95))
-
 
     def moveBack(self):
         self.move(0.11, 0.93)
@@ -54,7 +49,6 @@ class Giochino:
         else:
             return True
 
-
     def accept(self):
         self.gtfo()
         accept = pyautogui.locateOnScreen('resources/accept.png')
@@ -63,7 +57,7 @@ class Giochino:
     def redeemPremium(self):
         self.moveToPrizes()
 
-        #money
+        # money
         self.click(0.8, 0.22)
         time.sleep(0.2)
         self.moveToPrizes()
@@ -74,7 +68,7 @@ class Giochino:
         time.sleep(0.2)
         self.moveToPrizes()
 
-        #module
+        # module
         self.click(0.8, 0.4)
         time.sleep(0.2)
         self.moveToPrizes()
@@ -85,7 +79,7 @@ class Giochino:
         time.sleep(0.2)
         self.moveToPrizes()
 
-        #supplyContainer
+        # supplyContainer
         self.click(0.8, 0.62)
         self.moveToPrizes()
         self.click(0.8, 0.62)
@@ -102,55 +96,55 @@ class Giochino:
     def singlePlayer(self):
         self.moveToPlanes()
         self.moveToPlanes()
-        #self.isOnPlane()
-        #click single player
-        self.click(0.5,0.79)
-        #select level 53
-        self.move(0.9,0.27)
-        self.click(0.9,0.27)
-        self.move(0.5,0.5)
+        # self.isOnPlane()
+        # click single player
+        self.click(0.5, 0.79)
+        # select level 53
+        self.move(0.9, 0.27)
+        self.click(0.9, 0.27)
+        self.move(0.5, 0.5)
         pyautogui.dragTo(self.windowX(0.8), self.windowY(0.5), 0.3)
-        self.move(0.5,0.5)
+        self.move(0.5, 0.5)
         pyautogui.dragTo(self.windowX(0.8), self.windowY(0.5), 0.3)
-        self.move(0.5,0.5)
+        self.move(0.5, 0.5)
         pyautogui.dragTo(self.windowX(0.8), self.windowY(0.5), 0.3)
-        self.move(0.5,0.5)
+        self.move(0.5, 0.5)
         pyautogui.dragTo(self.windowX(0.8), self.windowY(0.5), 0.3)
-        self.move(0.5,0.5)
+        self.move(0.5, 0.5)
         pyautogui.dragTo(self.windowX(0.8), self.windowY(0.5), 0.3)
-        self.move(0.5,0.5)
+        self.move(0.5, 0.5)
         pyautogui.dragTo(self.windowX(0.8), self.windowY(0.5), 0.3)
-        self.click(0.7,0.61)
+        self.click(0.7, 0.61)
         time.sleep(1)
-        self.click(0.25,0.37)
-        self.click(0.25,0.37)
+        self.click(0.25, 0.37)
+        self.click(0.25, 0.37)
         time.sleep(1)
-        #select difficulty
-        self.click(0.8,0.25)
-        #play
-        self.move(0.5,0.94)
-        self.click(0.5,0.94)
-        #click manual
-        self.click(0.27,0.73)
+        # select difficulty
+        self.click(0.8, 0.25)
+        # play
+        self.move(0.5, 0.94)
+        self.click(0.5, 0.94)
+        # click manual
+        self.click(0.27, 0.73)
 
     def dumbPlay(self):
-        self.move(0.2,0.9)
+        self.move(0.2, 0.9)
         time.sleep(0.4)
         for i in range(84):
-            pyautogui.dragTo(self.windowX(0.95), self.windowY(0.9),0.55)
-            pyautogui.dragTo(self.windowX(0.05), self.windowY(0.9),0.55)
+            pyautogui.dragTo(self.windowX(0.95), self.windowY(0.9), 0.55)
+            pyautogui.dragTo(self.windowX(0.05), self.windowY(0.9), 0.55)
             print(i)
         self.pressKey(0x51)
         print('q')
         for i in range(6):
-            pyautogui.dragTo(self.windowX(0.95), self.windowY(0.9),0.6)
-            pyautogui.dragTo(self.windowX(0.05), self.windowY(0.9),0.6)
+            pyautogui.dragTo(self.windowX(0.95), self.windowY(0.9), 0.6)
+            pyautogui.dragTo(self.windowX(0.05), self.windowY(0.9), 0.6)
             print(i)
         self.pressKey(0x45)
         print('e')
         for i in range(70):
-            pyautogui.dragTo(self.windowX(0.95), self.windowY(0.9),0.6)
-            pyautogui.dragTo(self.windowX(0.05), self.windowY(0.9),0.6)
+            pyautogui.dragTo(self.windowX(0.95), self.windowY(0.9), 0.6)
+            pyautogui.dragTo(self.windowX(0.05), self.windowY(0.9), 0.6)
             print(i)
         time.sleep(0.3)
         self.click(0.5, 0.59)
@@ -163,8 +157,7 @@ class Giochino:
         self.moveBack()
         time.sleep(1)
 
-
-    def pressKey(self,code):
+    def pressKey(self, code):
         keyboard.PressKey(code)
         time.sleep(0.2)
         keyboard.ReleaseKey(code)
@@ -173,9 +166,10 @@ class Giochino:
         moveTo(self.windowX(xPercent), self.windowY(yPercent))
         leftClick(self.windowX(xPercent), self.windowY(yPercent))
         time.sleep(0.4)
+
     def move(self, xPercent, yPercent):
         moveTo(self.windowX(xPercent), self.windowY(yPercent))
         time.sleep(0.4)
+
     def isOnPlane(self):
         pyautogui.locateOnScreen('resources/menu_planes_selected.png')
-
